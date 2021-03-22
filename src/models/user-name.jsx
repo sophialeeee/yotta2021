2d6edfa5a32f88ce5ecedfb7f067de4dce13ddd3
kris <1184646300@qq.com>
2021-03-22 10:57:11 +0800

    添加文件 models/user-name.jsx

---
+++ src/models/user-name.jsx
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+import {createModel} from 'hox';
+
+
+function useUserName() {
+    const [UserName, _setUserName] = useState();
+    const setUserName = (UserName) => {
+        _setUserName(UserName)        
+    };
+   
+
+    return {
+        UserName,
+        setUserName,  
+    };
+}
+
+export default createModel(useUserName);
+
