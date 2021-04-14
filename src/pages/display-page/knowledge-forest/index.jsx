7876a41b855fd55b02efb4b4036255240d85255f
Sophia.lee <583171726@qq.com>
2021-04-14 18:51:00 +0800

    haha

--- src/pages/display-page/knowledge-forest/index.jsx
+++ src/pages/display-page/knowledge-forest/index.jsx
@@ -60,7 +60,8 @@ function KnowledgeForest () {
     const res = await YottaAPI.getASsembleByFacetId(facetId);
     setassembles(res);
     const res1 = await YottaAPI.getFacetName1(facetId);
-    setfacetName(res1.facetName);
+    if (res1.facetName){
+    setfacetName(res1.facetName);}
   }
 
   async function clickTopic (topicId, topicName) {
@@ -87,7 +88,7 @@ function KnowledgeForest () {
   return (
     <>
       <Card title="知识森林概览" style={mapStyle}>
-        <div style={{ width: '700px', height: '700px' }}>
+        <div style={{ width: '100%', height: '700px' }}>
           <svg ref={ref => mapRef.current = ref} id='map' style={{ width: '100%', height: '100%' }}></svg>
           <svg ref={ref => treeRef.current = ref} id='tree' style={{
             position: 'absolute', left: '0', marginLeft: 28,
